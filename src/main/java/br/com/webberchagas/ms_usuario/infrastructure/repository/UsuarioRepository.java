bfd0c34debe4744b1faae9f80f0e7158a5aae9c1
webberchagas <webberschagas@gmail.com>
2026-01-27 13:25:07 -0300

    Adicionado as classes para o spring security

---
+++ src/main/java/br/com/webberchagas/ms_usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.com.webberchagas.ms_usuario.infrastructure.repository;
+
+import br.com.webberchagas.ms_usuario.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+        boolean existsByEmail(String email);
+
+        Optional<Usuario> findByEmail(String email);
+}
