3759721d219e31fe2f4370488dbf6988814823a5
webberchagas <webberschagas@gmail.com>
2026-01-27 12:55:54 -0300

    First commit

---
+++ src/test/java/br/com/webberchagas/ms_usuario/MsUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webberchagas.ms_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
